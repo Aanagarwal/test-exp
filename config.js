52f48eb4445a1d3b03d81684fd9c22fd6db36aa4
aanchal <aanchal.agarwal@alcortech.com>
2021-12-03 21:24:18 +0530

    Updated access of environment variables

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    aws_table_name: $TABLE_NAME,
+    aws_table_name: process.env.TABLE_NAME,
     aws_local_config: {
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: $ACCESS_KEY,
-      secretAccessKey: $SECRET_ACCESS_KEY,
-      region: $REGION,
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      region: process.env.REGION,
     }
 };
